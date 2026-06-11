b03af7e2fac2d65e6c2c8026b7489823d3e00ffb
Donzeker12 <donzeker1@gmail.com>
2026-06-11 19:50:17 +0200

    Update deploy script to use PHP 8.4

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 # =============================================================
 
 APP_DIR="/var/www/goedopwegnijkerk"
-PHP="php8.3"
+PHP="php8.4"
 COMPOSER="composer"
 
 echo "===> Deployment gestart: $(date)"
